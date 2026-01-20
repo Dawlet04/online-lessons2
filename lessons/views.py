9f118bb14503acbe72a2268aa8fbb9999c0e26e5
калекешова-01 <baht30724@gmail.com>
2026-01-20 15:42:17 +0500

    sasa

--- lessons/views.py
+++ lessons/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from .models import Course, Category, Teacher
 
-# Create your views here.
+def home(request):
+    popular_courses = Course.objects.filter(is_published=True).order_by('-created_at')[:6]
+    categories = Category.objects.all()
+    teachers = Teacher.objects.all()[:4]
+    context = {
+        'popular_courses': popular_courses,
+        'categories': categories,
+        'top_teachers': teachers,
+    }
+    
+    return render(request, 'base.html', context)
+
